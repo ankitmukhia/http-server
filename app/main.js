17f4e58e2057162420cd8d82a0d55aea0f652eb0
ankutmukhia <ankitmukhia157@gmail.com>
2025-04-05 15:49:16 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,13 +1,25 @@
 const net = require("net");
+const PORT = 4221;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const server = net.createServer((socket) => {
+	socket.on("data", (data) => {
+		const res = data.toString()
+		console.log("Result: ", res)
+	})
 
-// Uncomment this to pass the first stage
-/* const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
-});
+	socket.write('Hello from TCP server');
+	socket.end();
 
-server.listen(4221, "localhost"); */
+	socket.on("end", () => {
+		console.log(`Client disconnected`)
+	})
+
+	socket.on("error", (error) => {
+		console.log("Socket Errror " + error)
+	})
+})
+
+server.listen(PORT, () => {
+	console.log(`TCP is listening at port ${PORT}`)
+})
+
