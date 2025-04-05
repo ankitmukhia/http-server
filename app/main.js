cf097a90656a5ac39458407205136c070dbf9e71
ankutmukhia <ankitmukhia157@gmail.com>
2025-04-05 19:00:56 +0530

    extracted url path

--- app/main.js
+++ app/main.js
@@ -2,14 +2,30 @@ const net = require("net");
 const PORT = 4221;
 
 const server = net.createServer((socket) => {
-	const httpResponse = "HTTP/1.1 200 OK\r\n\r\n"
-	console.log("http res: ", httpResponse)
+	socket.on('data', (data) => {
+		const res = data.toString();
+		const url = res.split(' ')[1]
 
-	socket.write(httpResponse);
-	socket.end();
+		switch (url) {
+			case "/abcdefg":
+				const httpResponse404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+				socket.write(httpResponse404);
+				socket.end();
+				break;
+			default:
+				const httpResponse200 = "HTTP/1.1 200 OK\r\n\r\n";
+				socket.write(httpResponse200);
+				socket.end();
+		}
+	})
 
 	socket.on("end", () => {
-		console.log(`Client disconnected`)
+		socket.end();
+	})
+
+	socket.on('error', (error) => {
+		console.log("error: ", error)
+		socket.end()
 	})
 })
 
