91dfb7716432f77ce3e4edbcfa721d90fb231f52
ankutmukhia <ankitmukhia157@gmail.com>
2025-04-05 16:13:34 +0530

    something done

--- app/main.js
+++ app/main.js
@@ -2,20 +2,14 @@ const net = require("net");
 const PORT = 4221;
 
 const server = net.createServer((socket) => {
-	socket.on("data", (data) => {
-		const res = data.toString()
-		console.log("Result: ", res)
-	})
+	const httpResponse = "HTTP/1.1 200 OK \r\n \r\n"
+	console.log("http res: ", httpResponse)
 
-	socket.write('HTTP/1.1');
+	socket.write(httpResponse);
 	socket.end();
 
 	socket.on("end", () => {
 		console.log(`Client disconnected`)
-	})
-
-	socket.on("error", (error) => {
-		console.log("Socket Errror " + error)
 	})
 })
 
