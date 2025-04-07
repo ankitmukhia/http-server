8471f5acfde04b9c5e66a25dc16d50e2a1123d55
ankutmukhia <ankitmukhia157@gmail.com>
2025-04-08 01:40:26 +0530

    read header fix

--- app/main.js
+++ app/main.js
@@ -4,6 +4,25 @@ const PORT = 4221;
 const server = net.createServer((socket) => {
 	socket.on('data', (data) => {
 		const res = data.toString();
+		// parse it in a way such that i can extract secific header
+		const [headers, body] = res.split('\r\n\r\n')
+		const lines = headers.split('\r\n')
+		let agentVal = "";
+
+		console.log("agentVal before: ", agentVal)
+
+		// extract headers
+		for (const line of lines.slice(1)) {
+			const [key, value] = line.split(': ')
+			if (key && value) {
+				if (key.toLowerCase().includes('user-agent')) {
+					agentVal = value
+				}
+			}
+		}
+
+		console.log("agentVal after: ", agentVal)
+
 		const url = res.split(' ')[1]
 
 		const echoMatch = /^\/echo\/[^/]+$/.test(url) ? '/__ECHO__' : url;
@@ -19,6 +38,11 @@ const server = net.createServer((socket) => {
 				const urlLength = formatedUrl.length;
 				const httpEchoResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${urlLength}\r\n\r\n${formatedUrl}`
 				socket.write(httpEchoResponse);
+				socket.end();
+				break;
+			case '/user-agent':
+				const httpAgentResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\n${agentVal}`
+				socket.write(httpAgentResponse);
 				socket.end();
 				break;
 			default:
