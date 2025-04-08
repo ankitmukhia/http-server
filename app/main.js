e810eeadbbd7bdabf7feb8f2ca9b66f733b2d78b
ankutmukhia <ankitmukhia157@gmail.com>
2025-04-08 18:58:14 +0530

    read file fix

--- app/main.js
+++ app/main.js
@@ -1,14 +1,12 @@
-const net = require("net");
-const fs = require('fs')
+const net = require("net")
+const fs = require("fs")
 const PORT = 4221;
 
-const rFile = fs.readFileSync('/tmp/foo', 'utf8', (err, data) => {
-	if (err) return console.error(err)
-	return data;
-})
-console.log("rFile: ", rFile)
-const fSizeBytes = fs.statSync('/tmp/foo')?.size
-console.log("fSizeBytes: ", fSizeBytes)
+// Get directory from the command line
+const dirFlagIndex = process.argv.indexOf('--directory')
+const dirPath = process.argv[dirFlagIndex + 1]
+
+fs.mkdirSync(dirPath, { recursive: true })
 
 // need to write file, user will send 'Hello, World' with path. ou have to send back the file
 const server = net.createServer((socket) => {
@@ -34,29 +32,33 @@ const server = net.createServer((socket) => {
 
 		const url = res.split(' ')[1]
 
-		const echoMatch = /^\/echo\/[^/]+$/.test(url) ? '/__ECHO__' : url;
+		// const echoMatch = /^\/echo\/[^/]+$/.test(url) ? '/__ECHO__' : url;
+		const rMatch = /^\/(files|echo)(\/[^/]+)?$/.test(url) ? '/__ECHO__' : url;
 
-		switch (echoMatch) {
+		switch (rMatch) {
 			case "/":
 				const httpResponse200 = "HTTP/1.1 200 OK\r\n\r\n";
 				socket.write(httpResponse200);
 				socket.end();
 				break;
 			case "/__ECHO__":
-				const formatedUrl = url.slice('/echo/'.length);
-				const urlLength = formatedUrl.length;
-				const httpEchoResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${urlLength}\r\n\r\n${formatedUrl}`
-				socket.write(httpEchoResponse);
-				socket.end();
-				break;
+				if (url === "/echo/") {
+					const formatedUrl = url.slice('/echo/'.length);
+					const urlLength = formatedUrl.length;
+					const httpEchoResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${urlLength}\r\n\r\n${formatedUrl}`
+					socket.write(httpEchoResponse);
+					socket.end();
+					break;
+				} else {
+					const formatedFilePath = url.slice('/files/'.length);
+					const rFile = fs.readFileSync(`${dirPath}/${formatedFilePath}`, 'utf8')
+					const fSizeBytes = fs.statSync(`${dirPath}/${formatedFilePath}`).size
+					const httpFileResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fSizeBytes}\r\n\r\n${rFile}`
+					break;
+				}
 			case '/user-agent':
 				const httpAgentResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${agentVal.length}\r\n\r\n${agentVal}`
 				socket.write(httpAgentResponse);
-				socket.end();
-				break;
-			case '/files/foo':
-				const httpFileResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fSizeBytes}\r\n\r\n${rFile}`
-				socket.write(httpFileResponse);
 				socket.end();
 				break;
 			default:
